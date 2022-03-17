1095c8a841191731225e4d0919f04b7d161967a1
Satyabrata das <t.satyabrata.das@e-arc.com>
2022-03-17 15:39:56 +0530

    third

---
+++ Clima/Model/weatherManager.swift
@@ -0,0 +1,41 @@
+//
+//  weatherManager.swift
+//  Clima
+//
+//  Created by ARC on 17/03/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager{
+    let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?appid=3a4fba8287927297777925634f855b76&units=metric"
+    
+    func fetchWeather(cityName:String){
+        let urlString = "\(weatherUrl)&q=\(cityName)"
+//        print(urlString)
+        pRequest(urlString: urlString)
+        
+    }
+    
+    func pRequest(urlString:String){
+        if let url = URL(string: urlString){
+            let Session = URLSession(configuration: .default)
+            
+            let task = Session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            
+            task.resume()
+        }
+    }
+    
+    func handle(data:Data?,response:URLResponse?,error:Error?){
+        if error != nil{
+            print(error!)
+            return
+        }
+        if let safedata = data{
+            let dataString = String(data:safedata,encoding: .utf8)
+            print(dataString)
+        }
+    }
+}
